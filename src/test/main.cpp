f39d4234b1470c277bbb1169c4054b58897b96a8
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-11-04 10:11:19 +0100

    Slower claw movement

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -5,10 +5,29 @@
 #define PWM_FREQ 50
 #define PWM_RES 16
 
+int currentAngle = 90;
+
 void setServoAngle(int angle) {
     int pulseWidth = map(angle, 0, 180, 1000, 2000);
     int duty = (pulseWidth * (1 << PWM_RES)) / 20000;
     ledcWrite(PWM_CHANNEL, duty);
+}
+
+void moveToAngle(int target) {
+    int stepSize = 1;
+    int stepDelay = 30;
+    if (currentAngle < target) {
+        for (int pos = currentAngle; pos <= target; pos += stepSize) {
+            setServoAngle(pos);
+            delay(stepDelay);
+        }
+    } else if (currentAngle > target) {
+        for (int pos = currentAngle; pos >= target; pos -= stepSize) {
+            setServoAngle(pos);
+            delay(stepDelay);
+        }
+    }
+    currentAngle = target;
 }
 
 void setup() {
@@ -16,14 +35,14 @@ void setup() {
     ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RES);
     ledcAttachPin(SERVO_PIN, PWM_CHANNEL);
     setServoAngle(90);
+    currentAngle = 90;
 }
 
 void loop() {
     for (int i = 0; i < 3; i++) {
-        Serial.println(i + 1);
-        setServoAngle(110);
-        delay(1000);
-        setServoAngle(70);
+        moveToAngle(115);
+        delay(800);
+        moveToAngle(65);
         delay(1000);
     }
 }
